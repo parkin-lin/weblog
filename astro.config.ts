40bb0ecb35d050058ad4e6e7fdcd2f17280c31fb
parkin-lin <parkin-lin@foxmail.com>
2025-03-14 17:44:45 +0800

    feat(astro.config.ts): add rehype-external-links
    modify links in Markdown pointing to external sites; adds icons to the end of each external link
    letting visitors know they'll leave.

--- astro.config.ts
+++ astro.config.ts
@@ -4,8 +4,21 @@ import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import UnoCSS from 'unocss/astro'
 import react from '@astrojs/react'
+import rehypeLinks from 'rehype-external-links'
 
 export default defineConfig({
   site: 'https://example.com',
   integrations: [react(), mdx(), sitemap(), UnoCSS({ injectReset: true })],
+  markdown: {
+    rehypePlugins: [
+      [
+        rehypeLinks,
+        {
+          content: { type: 'text', value: ' 🔗' },
+          target: '_blank',
+          rel: ['nofollow', 'noreferrer', 'noopener'],
+        },
+      ],
+    ],
+  },
 })
