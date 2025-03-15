79b243f536a4954f8d42c28becf185dbacd8dc92
parkin-lin <parkin-lin@foxmail.com>
2025-03-16 03:58:39 +0800

    feat(root): add icon preset and iconify

--- astro.config.ts
+++ astro.config.ts
@@ -6,6 +6,8 @@ import UnoCSS from 'unocss/astro'
 import react from '@astrojs/react'
 import rehypeLinks from 'rehype-external-links'
 
+const isDev = import.meta.env.DEV
+
 export default defineConfig({
   site: 'https://example.com',
   integrations: [react(), mdx(), sitemap(), UnoCSS({ injectReset: true })],
@@ -14,11 +16,17 @@ export default defineConfig({
       [
         rehypeLinks,
         {
-          content: { type: 'text', value: ' 🔗' },
+          content: {
+            type: 'text',
+            value: ' 🔗',
+          },
           target: '_blank',
           rel: ['nofollow', 'noreferrer', 'noopener'],
         },
       ],
     ],
+  },
+  devToolbar: {
+    enabled: isDev,
   },
 })
--- uno.config.ts
+++ uno.config.ts
@@ -1,6 +1,25 @@
-import { transformerDirectives } from 'unocss'
-import { defineConfig } from 'unocss'
+import {
+  defineConfig,
+  presetIcons,
+  presetWind3,
+  transformerDirectives,
+} from 'unocss'
 
+/**
+ * https://unocss.dev/integrations/astro#notes
+ * client:only components must be placed in components folder or added to UnoCSS's content config in order to be processed.
+ */
 export default defineConfig({
   transformers: [transformerDirectives()],
+  presets: [
+    presetWind3(),
+    presetIcons({
+      extraProperties: {
+        display: 'inline-block',
+        'vertical-align': 'middle',
+        height: '2em',
+        width: '2em',
+      },
+    }),
+  ],
 })
